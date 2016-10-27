557db5acf9dd90fa2bbac5fb2c14c806bf4e41c5
pj <starsinmypockets@gmail.com>
2016-10-27 10:28:31 -0400

    Get required params.

--- app.js
+++ app.js
@@ -2,7 +2,17 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Welcome to our super-simple test harness. Visit thisUrl.com/:DASH_VERSION/:GIST_SETTINGS_ID/:GIST_FUNCTIONS_FILE_ID to see your React-Dash!');
+});
+
+app.get('/:v/:gid1/:gid2', function (req, res) {
+  console.log(req.params);
+  var user = req.params.user || 'Unknown';
+  res.send('<ul>\
+              <li>DASH VERSION :   ' + req.params.v      + '</li>\
+              <li>SETTINGS ID  :   ' + req.params.gid1   + '</li>\
+              <li>FUNCTIONS ID :   ' + req.params.gid2   + '</li>\
+            </ul>');
 });
 
 app.listen(3000, function () {
