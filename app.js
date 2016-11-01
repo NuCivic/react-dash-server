24fff01d2341d135e886381e0a29642e21720451
pj <starsinmypockets@gmail.com>
2016-11-01 16:57:04 -0400

    Remove functions param. Load css. Checpoint. This works with first two tutorial settings.

--- app.js
+++ app.js
@@ -9,21 +9,21 @@ app.get('/', function (req, res) {
   res.render('pages/index', { title: 'Hey', message: 'Hello there!'})
 });
 
-app.get('/:v/:user/:gid1/:gid2', function (req, res) {
-  console.log(req.params);
+app.get('/:v/:user/:gid', function (req, res) {
+  var renderOpts = JSON.parse(JSON.stringify(req.params));
   var basePath = 'gist.githubusercontent.com';
   var body = []; 
   var opts = {}; 
+  
+  // fetch settings gist
   opts.host = basePath;
-  opts.path = path.join('/',req.params.user, req.params.gid1, 'raw');
+  opts.path = path.join('/',req.params.user, req.params.gid, 'raw');
   opts.method = 'GET';
   opts.headers = { 
     Accept: '*/*',
     'User-Agent': 'curl/7.47.0'
   };  
   
-  console.log(opts);
-
   var request = https.request(opts, (gist1) => {
     gist1.on('data', (chunk) => {
       body.push(chunk);
@@ -31,25 +31,27 @@ app.get('/:v/:user/:gid1/:gid2', function (req, res) {
 
     gist1.on('end', () => {
       var payload = Buffer.concat(body).toString();
-      console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
+      console.log('>>>>>>>>>>>>> SETTINGS <<<<<<<<<<<<<<');
       console.log(payload);
+      renderOpts.dashSettings = JSON.stringify(JSON.parse(payload));
+      renderOpts.test = JSON.stringify({foo: 'foo', bar: 'bar'});
+      res.render('pages/dash', renderOpts);
     }); 
   
     gist1.on('error', function (err) {
+      console.log('>>>> FETCH ERR <<<<');
       console.log(err)
     }); 
   });
 
   request.end();
 
-  res.render('pages/dash', req.params);
+});
+
+app.get('/react-dashboard', (req, res) => {
+  res.render('pages/demo');
 });
 
 app.listen(3333, function () {
   console.log('Example app listening on port 3333!');
 });
-
-/**** UTILITY ****/
-function getGist(opts) {
-  
-}
